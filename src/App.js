d3263fec58748a6aafff6b35e655a8193c1bf866
Luciana Albuquerque <luciana.albuquerque@studiographene.com>
2022-05-05 16:12:18 +0100

    wip firebase

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
+import './client/assets/styles/_styles.scss'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 import LandingPage from './client/components/views/LandingPage';
 import Navbar from './client/components/containers/Navbar';
 import Footer from './client/components/containers/Footer';
-import './client/assets/styles/_styles.scss'
+import LoginPage from './client/components/views/LandingPage/LoginPage';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Navbar />
-      <LandingPage />
+      <Route exact path='/' component={LandingPage} /> {/* //exact? */}
+      <Route path='/login' component={LoginPage} />
       <Footer />
     </div>
+    </Router>
   );
 }
 
