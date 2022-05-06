0e11b56fc287d396f8cd88f7e093c3507de5d0a6
Luciana Albuquerque <luciana.albuquerque@studiographene.com>
2022-05-06 09:55:53 +0100

    wip firebase

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import './client/assets/styles/_styles.scss'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-
-import LandingPage from './client/components/views/LandingPage';
 import Navbar from './client/components/containers/Navbar';
 import Footer from './client/components/containers/Footer';
-import LoginPage from './client/components/views/LandingPage/LoginPage';
+import LandingPage from './client/components/views/LandingPage';
+import LoginPage from './client/components/views/LoginPage';
+import RegisterPage from './client/components/views/RegisterPage';
 
 function App() {
   return (
     <Router>
     <div className="App">
       <Navbar />
-      <Route exact path='/' component={LandingPage} /> {/* //exact? */}
+      <Route exact path='/' component={LandingPage} />
+      <Route path='/register' component={RegisterPage} />
       <Route path='/login' component={LoginPage} />
       <Footer />
     </div>
--- src/client/components/views/LoginPage/index.js
+++ src/client/components/views/LoginPage/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function LoginPage() {
   return (
-    <div>LoginPage</div>
+    <div>Login Page</div>
   )
 }
