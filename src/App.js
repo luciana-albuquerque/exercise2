b93d52119d721da8e6c974d5286aaa64668901a9
Luciana Albuquerque <luciana.albuquerque@studiographene.com>
2022-05-06 15:14:33 +0100

    Revert "wip productStoreContext"
    This reverts commit ae84fb0549263817565b8af97403bda0ec628ac3.

--- src/App.js
+++ src/App.js
@@ -2,18 +2,15 @@ import LandingPage from './client/components/views/LandingPage';
 import Navbar from './client/components/containers/Navbar';
 import Footer from './client/components/containers/Footer';
 import './client/assets/styles/_styles.scss';
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import CartContext from './client/context/CartContext';
 
 function App() {
   const [cart, setCart] = useState([])
-  const [products, setProducts] = useState([])
 
   useEffect(() => {
     const cartItems = JSON.parse(localStorage.getItem('cart'))
     if (cartItems) setCart(cartItems)
-    const allProducts = JSON.parse(localStorage.getItem('products'))
-    if (allProducts) setProducts(allProducts)
   }, [])
 
   return (
